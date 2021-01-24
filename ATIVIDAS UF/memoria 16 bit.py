d10aeaf8e5c440a353c67cce2a79c670ef159f1a
Alfredo Gabriel <65864188+alverad-katsuro@users.noreply.github.com>
2021-01-24 18:15:19 -0300

    memo com cache

--- ATIVIDAS UF/memoria 16 bit.py
+++ ATIVIDAS UF/memoria 16 bit.py
@@ -1,45 +1,71 @@
 from time import sleep
+from random import choice
 
 memoria = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0, 15:0}
+memoria_cache = {}
+lista_keys = list(memoria.keys())
 
 def leitura():
-    global memoria
     endereco = input("Digite o endereço de 4 bits: ")
     if len(endereco) == 4:
-        endereco = int(endereco,2)
-        print(f"O dado em {endereco} é {memoria[endereco]:08d}",)
+        endereco = int(endereco, 2)
+        if endereco in memoria_cache:
+            print(f"\033[33mDado na memória cache\nO dado em {endereco:04b} é {memoria_cache[endereco]:08b}")
+        else:
+            print(f"\033[35mO dado em está na MP {endereco:04b} é {memoria[endereco]:08b}")
     else:
-        print("Digite um endereço valido")
+        print("\033[31mDigite um endereço valido")
         leitura()
 
 
 def escrita():
-    global memoria
     endereco = input("Digite o endereço de 4 bits: ")
     celula = input("Digite o valor da célula 8 bits: ")
     if len(endereco) == 4:
-        endereco = int(endereco,2)
+        endereco = int(endereco, 2)
         if len(celula) == 8:
             celula = int(celula)
             memoria[endereco] = celula
         else:
-            print("Digite uma célula valida")
+            print("\033[31Digite uma célula valida")
     else:
-        print("Digite um endereço valido")
+        print("\033[31Digite um endereço valido")
+    memo_cache()
+
+
+def memo_cache():
+    global lista_keys
+    par = {}
+    impa = {}
+    while (len(par) < 4) or (len(impa) < 4):
+        escolha = choice(lista_keys)
+        if (escolha % 2 == 0) and len(par) < 4:
+            if escolha not in par:
+                par.update({escolha:memoria[escolha]})
+        else:
+            if escolha not in impa and len(impa) < 4:
+                impa.update({escolha:memoria[escolha]})
+    memoria_cache.update(par)
+    memoria_cache.update(impa)
 
 
 def ler_tudo():
-    global memoria
-    inicio = 0
-    while inicio <= 15:
-        print(f"{memoria[inicio]:08d}")
-        inicio += 1
+    print("{:-^30}".format("Memória Principal"))
+    for k in range(len(memoria)):
+        print(f"Endereço {k:04b} valor {memoria[k]:08b}")
+    print("{:-^30}".format("Memória Cache"))
+    for keys in memoria_cache.keys():
+        bina = f'{keys:04b}'
+        tag = bina[:3]
+        conj = bina[3:4]
+        print(f"Tag: {tag} Conjunto: {conj} Valor: {memoria_cache[keys]:08b}")
     sleep(5)
 
 
 controle = "w"
+memo_cache()
 while (controle == "W") or (controle == "w") or (controle == "R") or (controle == "r") or (controle == "L") or (controle == "l"):
-    controle = input("Digite:\nW para escrever\nR para ler\nL para ler tudo\nQualquer tecla para parar\n")
+    controle = input("\033[34mDigite:\nW para escrever\nR para ler\nL para ler tudo\nQualquer tecla para parar\n")
     if (controle == "W") or (controle == "w"):
         escrita()
     elif (controle == "R") or (controle == "r"):
