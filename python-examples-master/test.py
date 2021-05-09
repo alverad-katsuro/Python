6ba3cc604fd9cde3ee012fcf17bbf6cd944e8c38
Alfredo Gabriel <65864188+alverad-katsuro@users.noreply.github.com>
2021-05-09 15:59:51 -0300

    main

---
+++ python-examples-master/test.py
@@ -0,0 +1,11 @@
+def soma(x: int, y: int) -> int:
+    print(x + y)
+    return x + y
+
+def testa (*valor):
+    print(valor)
+
+
+@f1(5)
+@f2
+def func(): pass
--- test.py
+++
@@ -1,8 +0,0 @@
-continuar = 1
-count = 0
-while continuar ==1:
-    x = continuar =int(input("Digite uma sequencia de algarismos para saber a quanditade de numeros primos nela contido\n"))
-    for aqui in reversed(range(1,x+1)):
-        if all(aqui%d !=0 for d in range(2,aqui)):
-            count += 1
-    print(count)
